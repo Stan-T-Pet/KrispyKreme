11886ae92d6ac47cc6f490a4ea732ee837cca3e4
Stan-T-Pet <114077038+Stan-T-Pet@users.noreply.github.com>
2024-12-13 05:02:39 +0000

    database target set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      DB_ADDRESS: process.env.DB_ADDRESS,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+  };
+  
+  export default nextConfig;  
