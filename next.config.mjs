377270d21159fa95a5d35bd286144b3e329b1c27
Stan-T-Pet <114077038+Stan-T-Pet@users.noreply.github.com>
2024-12-15 15:41:01 +0000

    Env variabkes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+import { createRequire } from 'module'; // Import createRequire to enable require in ESM
+const require = createRequire(import.meta.url);
+
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-      DB_ADDRESS: process.env.DB_ADDRESS,
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    },
-  };
-  
-  export default nextConfig;  
+  reactStrictMode: true,
+  env: {
+    DB_ADDRESS: process.env.DB_ADDRESS,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  webpack: (config) => {
+    // Fix for validator or other Webpack compatibility
+    config.resolve.alias['validator'] = require.resolve('validator');
+    return config;
+  },
+};
+
+export default nextConfig;
